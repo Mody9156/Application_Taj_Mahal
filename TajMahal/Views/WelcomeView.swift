a894920b1406656d990f1b2a213c5f14d90b1b71
Modibo KEITA <modykeita2@gmail.com>
2024-01-25 14:12:41 +0100

    add New folder

---
+++ TajMahal/Views/WelcomeView.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeView.swift
+//  TajMahal
+//
+//  Created by Amandine Cousin on 31/10/2023.
+//
+
+import SwiftUI
+
+// Page d'accueil
+struct WelcomeView: View {
+    var body: some View {
+        NavigationStack {
+            Image("TajMahal")
+            Spacer()
+            NavigationLink {
+                MenuView()
+            } label : {
+                Text("Menu")
+            }
+        }
+    }
+}
+
+struct WelcomeView_Preview : PreviewProvider{
+    static var previews :  some View {
+        WelcomeView()
+    }
+}
