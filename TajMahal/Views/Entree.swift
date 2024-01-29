beb88fcf8a1b550c9164ae76707a6f01e69c4268
Modibo KEITA <modykeita2@gmail.com>
2024-01-29 10:20:54 +0100

    création de deux views ainsi qu'une update de Menu Views

---
+++ TajMahal/Views/Entree.swift
@@ -0,0 +1,39 @@
+//
+//  Entree.swift
+//  TajMahal
+//
+//  Created by KEITA on 29/01/2024.
+//
+
+import SwiftUI
+
+struct Entree: View {
+    let viewModel: ViewModel = ViewModel()
+    
+    var body: some View {
+        Section("Entrées") {// titre de la setion
+            ForEach(viewModel.apetizerArray ,id:\.id){ apetizer in
+                
+                NavigationLink {
+                    Image("\(apetizer.imageName)").resizable().aspectRatio(contentMode: .fit).frame(width: 112,height: 86).cornerRadius(10)
+                } label: {
+                    HStack {
+                        Image("\(apetizer.imageName)").resizable().frame(width: 128.84,height: 86).cornerRadius(10)
+                        
+                        VStack(alignment: .leading) {
+                            Text(apetizer.name)
+                                .font(.title3).foregroundColor(.gray).fontWeight(.bold)
+                            Text(apetizer.description).foregroundColor(.gray)
+                            HStack{
+                                Text("\(apetizer.price,format:.number.precision(.fractionLength(2)))€")//format number permet de manipuler l'affichage du nombre
+                                Spacer()
+                                Image("\(apetizer.spiceLevel)")
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+}
---
+++ TajMahal/Views/Plats.swift
@@ -0,0 +1,38 @@
+//
+//  Plats.swift
+//  TajMahal
+//
+//  Created by KEITA on 29/01/2024.
+//
+
+import SwiftUI
+
+struct Plats: View {
+    let viewModel: ViewModel = ViewModel()
+    var body: some View {
+        Section("Plats Principaux") {// titre de la setion
+            ForEach(viewModel.mainCourseArray ,id:\.id){ mainCourse in
+                NavigationLink {
+                    Image("\(mainCourse.imageName)").resizable().frame(width: 128.84,height: 86).cornerRadius(10)
+                } label: {
+                    
+                    HStack {
+                        Image("\(mainCourse.imageName)").resizable().frame(width: 112,height: 86).cornerRadius(10)
+                        
+                        VStack(alignment: .leading) {
+                            Text(mainCourse.name)
+                                .font(.title3).foregroundColor(.gray).fontWeight(.bold)
+                            Text(mainCourse.description).foregroundColor(.gray)
+                            HStack{
+                                Text("\(mainCourse.price,format:.number.precision(.fractionLength(2)))€")//format number permet de manipuler l'affichage du nombre
+                                Spacer()
+                                Image("\(mainCourse.spiceLevel)")
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+}
