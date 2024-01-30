b8dc3b1e639569f6bada630a88aa0c64cc514bb6
Modibo KEITA <modykeita2@gmail.com>
2024-01-30 16:24:55 +0100

    update

--- TajMahal/Views/WelcomeView.swift
+++ TajMahal/Views/WelcomeView.swift
@@ -10,47 +10,47 @@ import SwiftUI
 // Page d'accueil
 struct WelcomeView: View {
     
-    @State var name : String  = ""
+    @State var name :String  = ""
     @State var icone : String = ""
     @State var moreNews  : String = ""
     var body: some View {
-        NavigationStack {
-            VStack{
-                Image("TajMahal").frame(width: 335,height: 423)//picture for the view
-              
-                HStack {
-                    Spacer()
-                    VStack (alignment: .leading) {
-                       
-                        Text("Restaurant Indien").foregroundColor(.gray)
-                        Text("Taj Mahal").font(.title).fontWeight(.bold)
-                      
+        ScrollView {
+            NavigationStack {
+                VStack{
+                    Image("TajMahal").frame(width: 335,height: 423)//picture for the view
+                    
+                    HStack {
+                        Spacer()
+                        VStack (alignment: .leading) {
+                            
+                            Text("Restaurant Indien").foregroundColor(.gray)
+                            Text("Taj Mahal").font(.title).fontWeight(.bold)
+                            
+                        }
+                        
+                        Image("Logo").resizable().renderingMode(.template).foregroundStyle(.gray).frame(width: 40,height: 40)
+                        Spacer()
+                        
+                    }
+                    VStack(alignment: .leading) {
+                        BottomView(name: "Mardi", icone: "clock", moreNews: "11h30 - 14h30 · 18h30 - 22h00")
+                        BottomView(name: "Type de Service", icone: "globe", moreNews: "a emporter")
+                        BottomView(name: "12 Avenue de la Brique - 75010 Paris ", icone: "globe", moreNews: "")
+                        BottomView(name: "www.tajmahal.fr", icone: "globe", moreNews: "")
                     }
                     
-                    Image("Logo").resizable().renderingMode(.template).foregroundStyle(.gray).frame(width: 40,height: 40)
                     Spacer()
-                   
+                    NavigationLink {
+                        MenuView()
+                    } label : {
+                        ZStack {
+                            Rectangle().frame(width: 335,height: 40).cornerRadius(10).foregroundColor(.red)
+                            Text("Accéder au menu").fontWeight(.bold).foregroundColor(.white)
+                        }
+                        
+                    }.navigationTitle("")
                 }
-                VStack(alignment: .leading) {
-                    BottomView(name: "Mardi", icone: "clock", moreNews: "11h30 - 14h30 · 18h30 - 22h00")
-                    BottomView(name: "Type de Service", icone: "globe", moreNews: "a emporter")
-                    BottomView(name: "12 Avenue de la Brique - 75010 Paris ", icone: "globe", moreNews: "")
-                    BottomView(name: "www.tajmahal.fr", icone: "globe", moreNews: "")
-                }
-               
-               
             }
-           
-            Spacer()
-            NavigationLink {
-                MenuView()
-            } label : {
-                ZStack {
-                    Rectangle().frame(width: 335,height: 40).cornerRadius(10).foregroundColor(.red)
-                    Text("Accéder au menu").fontWeight(.bold).foregroundColor(.white)
-                }
-               
-            }.navigationTitle("")
         }
     }
 }
