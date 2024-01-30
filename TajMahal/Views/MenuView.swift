ac4c131db1945b0194948011d54afa68cc670694
Modibo KEITA <modykeita2@gmail.com>
2024-01-30 22:49:25 +0100

    update des menus

--- TajMahal/Views/MenuView.swift
+++ TajMahal/Views/MenuView.swift
@@ -11,14 +11,81 @@ import SwiftUI
 struct MenuView: View {
     // Référence vers le view model qui permet d'accéder aux tableaux d'entrées et de plats du menu
     let viewModel: ViewModel = ViewModel()
+    
     var body: some View {
+        
         NavigationStack {
+            
             VStack(alignment: .center) {
-                List {// utilisation de l'autre manière d'incrémenter un tableau avec foreach
+                
+                List {
                     
-                    Entree()
+                    Section("Entrées") {// titre de la setion
+                        
+                        ForEach(viewModel.apetizerArray ,id:\.id){ apetizer in
+                            
+                            NavigationLink {
+                                
+                                ScrollView {
+                                    
+                                    VStack {
+                                        
+                                        Spacer()
+                                        
+                                        Image("\(apetizer.imageName)").resizable().frame(width: 335,height: 467).cornerRadius(10)
+                                        
+                                        VStack(alignment: .leading) {
+                                            
+                                            Spacer()
+                                            
+                                            Text("Allergènes:").foregroundColor(.gray).fontWeight(.bold).padding()
+                                            
+                                            Text(apetizer.allergens).foregroundColor(.gray)
+                                            
+                                            Spacer()
+                                            
+                                            Text("Ingredients:").foregroundColor(.gray).fontWeight(.bold).padding()
+                                            
+                                            Text(apetizer.ingredients).foregroundColor(.gray)
+                                            
+                                            Spacer()
+                                            
+                                        }.padding()
+                                        
+                                    }.navigationTitle(apetizer.imageName)
+                                }
+                            } label: {
+                                
+                                HStack {
+                                    
+                                    Image("\(apetizer.imageName)").resizable().frame(width: 112,height: 86).cornerRadius(10)
+                                    
+                                    VStack(alignment: .leading) {
+                                        
+                                        Text(apetizer.name)
+                                            .font(.title3).foregroundColor(.gray).fontWeight(.bold)
+                                        
+                                        Text(apetizer.description).foregroundColor(.gray)
+                                        
+                                        HStack{
+                                            
+                                            Text("\(apetizer.price,format:.number.precision(.fractionLength(2)))€")//format number permet de manipuler l'affichage du nombre
+                                            
+                                            Spacer()
+                                            
+                                            Image("\(apetizer.spiceLevel)")
+                                        }
+                                    }
+                                    Spacer()
+                                }
+                            }
+                            
+                        }.navigationTitle("")
+                        
+                    }
+//                    Entree()//incrémentation des entrées sous fourme de liste
                     
-                    Plats()
+                    Plats()//incrémentation des plats sous fourme de liste
                     
                 }
             }.navigationBarTitle("Menus")
@@ -30,8 +97,11 @@ struct MenuView: View {
 }
 
 struct MenuView_Preview : PreviewProvider{
+    
     static var previews :  some View {
-        MenuView()
+       
+            MenuView()
+    
     }
 }
 //#Preview{
