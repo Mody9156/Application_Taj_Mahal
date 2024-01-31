ce4e4afdd2bef307851b34e5412dedfd9366a697
Modibo KEITA <modykeita2@gmail.com>
2024-01-31 10:35:39 +0100

    update

--- TajMahal/Views/Entree.swift
+++ TajMahal/Views/Entree.swift
@@ -10,6 +10,68 @@ import SwiftUI
 struct Entree: View {
     let viewModel: ViewModel = ViewModel()
     var body: some View {
-        Text("o")
+        Section("Entrées") {// titre de la setion
+            
+            ForEach(viewModel.apetizerArray ,id:\.id){ apetizer in
+                
+                NavigationLink {
+                    
+                    ScrollView {
+                        
+                        VStack {
+                            
+                            Spacer()
+                            
+                            Image("\(apetizer.imageName)").resizable().frame(width: 335,height: 467).cornerRadius(10)
+                            
+                            VStack(alignment: .leading) {
+                                
+                                Spacer()
+                                
+                                Text("Allergènes:").foregroundColor(.gray).fontWeight(.bold).padding()
+                                
+                                Text(apetizer.allergens).foregroundColor(.gray)
+                                
+                                Spacer()
+                                
+                                Text("Ingredients:").foregroundColor(.gray).fontWeight(.bold).padding()
+                                
+                                Text(apetizer.ingredients).foregroundColor(.gray)
+                                
+                                Spacer()
+                                
+                            }.padding()
+                            
+                        }.navigationTitle(apetizer.imageName)
+                    }
+                } label: {
+                    
+                    HStack {
+                        
+                        Image("\(apetizer.imageName)").resizable().frame(width: 112,height: 86).cornerRadius(10)
+                        
+                        VStack(alignment: .leading) {
+                            
+                            Text(apetizer.name)
+                                .font(.title3).foregroundColor(.gray).fontWeight(.bold)
+                            
+                            Text(apetizer.description).foregroundColor(.gray)
+                            
+                            HStack{
+                                
+                                Text("\(apetizer.price,format:.number.precision(.fractionLength(2)))€")//format number permet de manipuler l'affichage du nombre
+                                
+                                Spacer()
+                                
+                                Image("\(apetizer.spiceLevel)")
+                            }
+                        }
+                        Spacer()
+                    }
+                }
+                
+            }.navigationTitle("")
+            
+        }
     }
 }
--- TajMahal/Views/MenuView.swift
+++ TajMahal/Views/MenuView.swift
@@ -20,70 +20,8 @@ struct MenuView: View {
                 
                 List {
                     
-                    Section("Entrées") {// titre de la setion
-                        
-                        ForEach(viewModel.apetizerArray ,id:\.id){ apetizer in
-                            
-                            NavigationLink {
-                                
-                                ScrollView {
-                                    
-                                    VStack {
-                                        
-                                        Spacer()
-                                        
-                                        Image("\(apetizer.imageName)").resizable().frame(width: 335,height: 467).cornerRadius(10)
-                                        
-                                        VStack(alignment: .leading) {
-                                            
-                                            Spacer()
-                                            
-                                            Text("Allergènes:").foregroundColor(.gray).fontWeight(.bold).padding()
-                                            
-                                            Text(apetizer.allergens).foregroundColor(.gray)
-                                            
-                                            Spacer()
-                                            
-                                            Text("Ingredients:").foregroundColor(.gray).fontWeight(.bold).padding()
-                                            
-                                            Text(apetizer.ingredients).foregroundColor(.gray)
-                                            
-                                            Spacer()
-                                            
-                                        }.padding()
-                                        
-                                    }.navigationTitle(apetizer.imageName)
-                                }
-                            } label: {
-                                
-                                HStack {
-                                    
-                                    Image("\(apetizer.imageName)").resizable().frame(width: 112,height: 86).cornerRadius(10)
-                                    
-                                    VStack(alignment: .leading) {
-                                        
-                                        Text(apetizer.name)
-                                            .font(.title3).foregroundColor(.gray).fontWeight(.bold)
-                                        
-                                        Text(apetizer.description).foregroundColor(.gray)
-                                        
-                                        HStack{
-                                            
-                                            Text("\(apetizer.price,format:.number.precision(.fractionLength(2)))€")//format number permet de manipuler l'affichage du nombre
-                                            
-                                            Spacer()
-                                            
-                                            Image("\(apetizer.spiceLevel)")
-                                        }
-                                    }
-                                    Spacer()
-                                }
-                            }
-                            
-                        }.navigationTitle("")
-                        
-                    }
-//                    Entree()//incrémentation des entrées sous fourme de liste
+                   
+                    Entree()//incrémentation des entrées sous fourme de liste
                     
                     Plats()//incrémentation des plats sous fourme de liste
                     
