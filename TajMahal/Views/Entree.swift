ba5ec0f3ad365a1e87330b39127fc86ededd35c2
Modibo KEITA <modykeita2@gmail.com>
2024-01-31 12:15:35 +0100

    update Entree and Plats

--- TajMahal/Views/Entree.swift
+++ TajMahal/Views/Entree.swift
@@ -12,66 +12,77 @@ struct Entree: View {
     var body: some View {
         Section("Entrées") {// titre de la setion
             
-            ForEach(viewModel.apetizerArray ,id:\.id){ apetizer in
-                
-                NavigationLink {
-                    
-                    ScrollView {
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.apetizerArray ,id:\.id){ apetizer in
                         
-                        VStack {
+                        NavigationLink {
                             
-                            Spacer()
-                            
-                            Image("\(apetizer.imageName)").resizable().frame(width: 335,height: 467).cornerRadius(10)
-                            
-                            VStack(alignment: .leading) {
+                            ScrollView {
                                 
-                                Spacer()
-                                
-                                Text("Allergènes:").foregroundColor(.gray).fontWeight(.bold).padding()
-                                
-                                Text(apetizer.allergens).foregroundColor(.gray)
-                                
-                                Spacer()
-                                
-                                Text("Ingredients:").foregroundColor(.gray).fontWeight(.bold).padding()
-                                
-                                Text(apetizer.ingredients).foregroundColor(.gray)
-                                
-                                Spacer()
-                                
-                            }.padding()
-                            
-                        }.navigationTitle(apetizer.imageName)
-                    }
-                } label: {
-                    
-                    HStack {
-                        
-                        Image("\(apetizer.imageName)").resizable().frame(width: 112,height: 86).cornerRadius(10)
-                        
-                        VStack(alignment: .leading) {
-                            
-                            Text(apetizer.name)
-                                .font(.title3).foregroundColor(.gray).fontWeight(.bold)
-                            
-                            Text(apetizer.description).foregroundColor(.gray)
-                            
-                            HStack{
-                                
-                                Text("\(apetizer.price,format:.number.precision(.fractionLength(2)))€")//format number permet de manipuler l'affichage du nombre
-                                
-                                Spacer()
-                                
-                                Image("\(apetizer.spiceLevel)")
+                                VStack {
+                                    
+                                    Spacer()
+                                    
+                                    Image("\(apetizer.imageName)").resizable().aspectRatio(contentMode: .fill).frame(width: 335,height: 467).cornerRadius(10)
+                                    
+                                    VStack(alignment: .leading) {
+                                        
+                                        Spacer()
+                                        
+                                        Text("Allergènes:").foregroundColor(.gray).fontWeight(.bold).padding()
+                                        
+                                        Text(apetizer.allergens).foregroundColor(.gray)
+                                        
+                                        Spacer()
+                                        
+                                        Text("Ingredients:").foregroundColor(.gray).fontWeight(.bold).padding()
+                                        
+                                        Text(apetizer.ingredients).foregroundColor(.gray)
+                                        
+                                        Spacer()
+                                        
+                                    }.padding()
+                                    
+                                }.navigationTitle(apetizer.imageName)
                             }
+                        } label: {
+                            Entreelabel(apetizer:apetizer)
+                          
                         }
-                        Spacer()
-                    }
+                        
+                    }.navigationTitle("")
                 }
-                
-            }.navigationTitle("")
+            }
             
+        }
+    }
+}
+
+struct Entreelabel : View {
+    var apetizer : Dish
+    var body: some View {
+        HStack {
+            
+            Image("\(apetizer.imageName)").resizable().frame(width: 112,height: 86).cornerRadius(10)
+            
+            VStack(alignment: .leading) {
+                
+                Text(apetizer.name)
+                    .font(.title3).foregroundColor(.gray).fontWeight(.bold)
+                
+                Text(apetizer.description).foregroundColor(.gray)
+                
+                HStack{
+                    
+                    Text("\(apetizer.price,format:.number.precision(.fractionLength(2)))€")//format number permet de manipuler l'affichage du nombre
+                    
+                    Spacer()
+                    
+                    Image("\(apetizer.spiceLevel)")
+                }
+            }
+            Spacer()
         }
     }
 }
--- TajMahal/Views/Plats.swift
+++ TajMahal/Views/Plats.swift
@@ -11,48 +11,36 @@ struct Plats: View {
     let viewModel: ViewModel = ViewModel()
     var body: some View {
         Section(header:Text("Plats Principaux")) {// titre de la setion
-            ForEach(viewModel.mainCourseArray ,id:\.id){ mainCourse in
-                NavigationLink {
-                
-                    ScrollView {
-                        VStack {
-                            Spacer()
-                                Image("\(mainCourse.imageName)").resizable().frame(width: 335,height: 467).cornerRadius(10)
-                                
-                                VStack(alignment: .leading) {
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.mainCourseArray ,id:\.id){ mainCourse in
+                        NavigationLink {
+                        
+                            ScrollView {
+                                VStack {
                                     Spacer()
-                                    Text("Allergènes:").foregroundColor(.gray).fontWeight(.bold).padding()
-                                    Text(mainCourse.allergens).foregroundColor(.gray)
-                                
-                                    Spacer()
-                                    Text("Ingredients:").foregroundColor(.gray).fontWeight(.bold).padding()
-                                       
-                                    Text(mainCourse.ingredients).foregroundColor(.gray)
-                                    Spacer()
-                                }.padding()
-                           
-                        }.navigationTitle(mainCourse.imageName)
-                    }
-                } label: {
-                    
-                    VStack {
-                        HStack {
-                            
-                            Image("\(mainCourse.imageName)").resizable().frame(width: 112,height: 86).cornerRadius(10)
-                            
-                            VStack(alignment: .leading) {
-                                Text(mainCourse.name)
-                                    .font(.title3).foregroundColor(.gray).fontWeight(.bold)
-                                Text(mainCourse.description).foregroundColor(.gray)
-                                HStack{
-                                    Text("\(mainCourse.price,format:.number.precision(.fractionLength(2)))€")//format number permet de manipuler l'affichage du nombre
-                                    Spacer()
-                                    Image("\(mainCourse.spiceLevel)").resizable().frame(width: 52, height: 12)
+                                        Image("\(mainCourse.imageName)").resizable().aspectRatio(contentMode: .fill).frame(width: 335,height: 467).cornerRadius(10)
+                                        
+                                        VStack(alignment: .leading) {
+                                            Spacer()
+                                            Text("Allergènes:").foregroundColor(.gray).fontWeight(.bold).padding()
+                                            Text(mainCourse.allergens).foregroundColor(.gray)
+                                        
+                                            Spacer()
+                                            Text("Ingredients:").foregroundColor(.gray).fontWeight(.bold).padding()
+                                               
+                                            Text(mainCourse.ingredients).foregroundColor(.gray)
+                                            Spacer()
+                                        }.padding()
+                                  
                                 }
-                            }
+                            }.navigationBarTitle(mainCourse.name)
+                        } label: {
+                            
+                            PlatsLabels(mainCourse:mainCourse)
                         }
                     }
-                }.navigationTitle("")
+                }
             }
             
         }
@@ -63,3 +51,26 @@ struct Plats: View {
 
 
 
+struct PlatsLabels : View {
+    var mainCourse : Dish
+    var body: some View {
+        VStack {
+            HStack {
+                
+                Image("\(mainCourse.imageName)").resizable().frame(width: 112,height: 86).cornerRadius(10)
+                
+                VStack(alignment: .leading) {
+                    Text(mainCourse.name)
+                        .font(.title3).foregroundColor(.gray).fontWeight(.bold)
+                    Text(mainCourse.description).foregroundColor(.gray)
+                    HStack{
+                        Text("\(mainCourse.price,format:.number.precision(.fractionLength(2)))€")//format number permet de manipuler l'affichage du nombre
+                        Spacer()
+                        Image("\(mainCourse.spiceLevel)").resizable().frame(width: 52, height: 12)
+                    }
+                }
+            }.environment(\.layoutDirection, .rightToLeft)
+                .environment(\.defaultMinListRowHeight, 150)
+        }
+    }
+}
