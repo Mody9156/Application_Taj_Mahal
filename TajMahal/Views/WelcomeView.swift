0bfd251e8ed6a2718362c91e88a10d063b36ae6e
Modibo KEITA <modykeita2@gmail.com>
2024-02-01 23:10:58 +0100

    update WelcomView

--- TajMahal/Views/WelcomeView.swift
+++ TajMahal/Views/WelcomeView.swift
@@ -14,69 +14,69 @@ struct WelcomeView: View {
     @State var icone : String = ""
     @State var moreNews  : String = ""
     var body: some View {
-      
-            ScrollView {
-                NavigationStack {
-                    VStack{
-                        Image("TajMahal").frame(width: 335,height: 423)//picture for the view
-
-                        HStack {
-                            Spacer()
-                            VStack (alignment: .leading) {
-
-                                Text("Restaurant Indien").foregroundColor(.gray)
-                                Text("Taj Mahal").font(.title).fontWeight(.bold)
-
-                            }
-
-                            Image("Logo").resizable().renderingMode(.template).foregroundStyle(.gray).frame(width: 40,height: 40)
-                            Spacer()
-
-                        }
-                        VStack(alignment: .leading) {
-                            BottomView(name: "Mardi", icone: "clock", moreNews: "11h30 - 14h30 · 18h30 - 22h00")
-                            BottomView(name: "Type de Service", icone: "globe", moreNews: "a emporter")
-                            BottomView(name: "12 Avenue de la Brique - 75010 Paris ", icone: "globe", moreNews: "")
-                            BottomView(name: "www.tajmahal.fr", icone: "globe", moreNews: "")
-                        }
-
-                        Spacer()
-                        NavigationLink {
-                            MenuView()
-                        } label : {
-                            VStack {
-                                Rectangle().frame(width: 335,height: 40).cornerRadius(10).foregroundColor(.red)
-                                Text("Accéder au menu").fontWeight(.bold).foregroundColor(.white)
-                            }
-
-                        }.navigationTitle("")
-                    }
-                }
-            }
+      Text("help")
+//            ScrollView {
+//                NavigationStack {
+//                    VStack{
+//                        Image("TajMahal").frame(width: 335,height: 423)//picture for the view
+//
+//                        HStack {
+//                            Spacer()
+//                            VStack (alignment: .leading) {
+//
+//                                Text("Restaurant Indien").foregroundColor(.gray)
+//                                Text("Taj Mahal").font(.title).fontWeight(.bold)
+//
+//                            }
+//
+//                            Image("Logo").resizable().renderingMode(.template).foregroundStyle(.gray).frame(width: 40,height: 40)
+//                            Spacer()
+//
+//                        }
+//                        VStack(alignment: .leading) {
+//                            BottomView(name: "Mardi", icone: "clock", moreNews: "11h30 - 14h30 · 18h30 - 22h00")
+//                            BottomView(name: "Type de Service", icone: "globe", moreNews: "a emporter")
+//                            BottomView(name: "12 Avenue de la Brique - 75010 Paris ", icone: "globe", moreNews: "")
+//                            BottomView(name: "www.tajmahal.fr", icone: "globe", moreNews: "")
+//                        }
+//
+//                        Spacer()
+//                        NavigationLink {
+//                            MenuView()
+//                        } label : {
+//                            VStack {
+//                                Rectangle().frame(width: 335,height: 40).cornerRadius(10).foregroundColor(.red)
+//                                Text("Accéder au menu").fontWeight(.bold).foregroundColor(.white)
+//                            }
+//
+//                        }.navigationTitle("")
+//                    }
+//                }
+//            }
         
     }
 }
 
-
-struct BottomView: View {
-
-    var name:String
-    var icone:String
-    var moreNews:String
-
-    var body: some View {
-
-        Group{
-            HStack {
-
-                Label(name, systemImage: icone).foregroundColor(.gray)
-
-
-                Text(moreNews).foregroundColor(.gray)
-
-
-            }.padding(5)
-
-        }
-    }
-}
+//
+//struct BottomView: View {
+//
+//    var name:String
+//    var icone:String
+//    var moreNews:String
+//
+//    var body: some View {
+//
+//        Group{
+//            HStack {
+//
+//                Label(name, systemImage: icone).foregroundColor(.gray)
+//
+//
+//                Text(moreNews).foregroundColor(.gray)
+//
+//
+//            }.padding(5)
+//
+//        }
+//    }
+//}
