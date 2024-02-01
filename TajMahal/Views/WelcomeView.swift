f641c07cd38494706968c8e7cbfcd9d158a1ca13
Modibo KEITA <modykeita2@gmail.com>
2024-02-01 16:36:24 +0100

    update

--- TajMahal/Views/WelcomeView.swift
+++ TajMahal/Views/WelcomeView.swift
@@ -15,8 +15,6 @@ struct WelcomeView: View {
     @State var moreNews  : String = ""
     var body: some View {
       
-        VStack {
-            Text("e")
             ScrollView {
                 NavigationStack {
                     VStack{
@@ -35,18 +33,18 @@ struct WelcomeView: View {
                             Spacer()
 
                         }
-//                        VStack(alignment: .leading) {
-//                            BottomView(name: "Mardi", icone: "clock", moreNews: "11h30 - 14h30 · 18h30 - 22h00")
-//                            BottomView(name: "Type de Service", icone: "globe", moreNews: "a emporter")
-//                            BottomView(name: "12 Avenue de la Brique - 75010 Paris ", icone: "globe", moreNews: "")
-//                            BottomView(name: "www.tajmahal.fr", icone: "globe", moreNews: "")
-//                        }
+                        VStack(alignment: .leading) {
+                            BottomView(name: "Mardi", icone: "clock", moreNews: "11h30 - 14h30 · 18h30 - 22h00")
+                            BottomView(name: "Type de Service", icone: "globe", moreNews: "a emporter")
+                            BottomView(name: "12 Avenue de la Brique - 75010 Paris ", icone: "globe", moreNews: "")
+                            BottomView(name: "www.tajmahal.fr", icone: "globe", moreNews: "")
+                        }
 
                         Spacer()
                         NavigationLink {
                             MenuView()
                         } label : {
-                            ZStack {
+                            VStack {
                                 Rectangle().frame(width: 335,height: 40).cornerRadius(10).foregroundColor(.red)
                                 Text("Accéder au menu").fontWeight(.bold).foregroundColor(.white)
                             }
@@ -55,30 +53,30 @@ struct WelcomeView: View {
                     }
                 }
             }
-        }
+        
     }
 }
 
-//
-//struct BottomView: View {
-//
-//    var name:String
-//    var icone:String
-//    var moreNews:String
-//
-//    var body: some View {
-//
-//        Group{
-//            HStack {
-//
-//                Label(name, systemImage: icone).foregroundColor(.gray)
-//
-//
-//                Text(moreNews).foregroundColor(.gray)
-//
-//
-//            }.padding(5)
-//
-//        }
-//    }
-//}
+
+struct BottomView: View {
+
+    var name:String
+    var icone:String
+    var moreNews:String
+
+    var body: some View {
+
+        Group{
+            HStack {
+
+                Label(name, systemImage: icone).foregroundColor(.gray)
+
+
+                Text(moreNews).foregroundColor(.gray)
+
+
+            }.padding(5)
+
+        }
+    }
+}
